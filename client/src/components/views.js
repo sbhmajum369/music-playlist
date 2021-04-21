330319092b7e8838ebd45dfd5a5ba3b24cb15ad4
sbhmajum369 <majum.subh@gmail.com>
2021-04-21 15:09:36 -0400

    V_0.1

--- client/src/components/views.js
+++
@@ -1,4 +0,0 @@
-
-export { default as Home } from "./home";
-export { default as PlayList } from "./playlist";
-export { default as Player } from "./player";
---
+++ server.js
@@ -0,0 +1,79 @@
+
+const express = require('express');
+const app = express();
+const fs = require('fs');
+const cors = require('cors');
+var path = require('path');
+
+const videoFolder = './assets/';
+const thumbFolder = `${__dirname}/public/`;
+var mediaFiles = [];
+
+app.use(cors());
+app.use(express.static(path.join(__dirname,'/build')));
+
+// Collecting video file name
+fs.readdir(videoFolder, (err, files) => {
+	files.forEach(file => {
+		mediaFiles.push(file.split('.')[0]);
+	});
+	// console.log(mediaFiles);
+});
+
+
+// Sending video names
+app.get('/video', (req, res) => res.json(mediaFiles));
+
+// Thumbnail pictures
+app.get('/video/:id/poster', (req, res) => {
+	// console.log(thumbFolder+`${req.params.id}`);
+	res.sendFile(thumbFolder+`${req.params.id}.jpg`);
+});
+
+// Streaming video data
+app.get('/video/:id', (req, res) => {
+	const path = videoFolder+`${req.params.id}.mp4`;
+	const stat = fs.statSync(path);
+	const fileSize = stat.size;
+	const range = req.headers.range;
+
+	if (range) {
+		const parts = range.replace(/bytes=/, "").split("-")
+		const start = parseInt(parts[0], 10)
+		const end = parts[1] ? parseInt(parts[1], 10) : fileSize-1;
+
+		if(start >= fileSize) {
+		  res.status(416).send('Requested range not satisfiable\n'+start+' >= '+fileSize);
+		}
+
+		const chunksize = (end-start)+1
+		const file = fs.createReadStream(path, {start, end})
+		const head = {
+		  'Content-Range': `bytes ${start}-${end}/${fileSize}`,
+		  'Accept-Ranges': 'bytes',
+		  'Content-Length': chunksize,
+		  'Content-Type': 'video/mp4',
+		}
+		res.writeHead(206, head);
+		file.pipe(res);
+	} else {
+		const head = {
+			'Content-Length': fileSize,
+			'Content-Type': 'video/mp4',
+			};
+		res.writeHead(200, head);
+		fs.createReadStream(path).pipe(res);
+	}
+
+});
+
+app.get('*', (req, res) => {
+	res.sendFile(path.join(__dirname + '/build/index.html'));
+});
+
+app.listen(4000, () => {
+	console.clear();
+    console.log('Listening on port 4000!');
+    console.log('Visit localhost:4000');
+});
+
